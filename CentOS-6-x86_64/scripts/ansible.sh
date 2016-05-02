c9ebd80b99bc6793f01cd95dd08df55bebb48137
Raymond Coetzee <coetzeer@gmail.com>
2016-05-02 08:38:32 +0100

    Use tag in ansible version

--- CentOS-6-x86_64/scripts/ansible.sh
+++ CentOS-6-x86_64/scripts/ansible.sh
@@ -5,6 +5,7 @@ pip install PyYAML jinja2 paramiko
 cd ~
 git clone https://github.com/ansible/ansible.git
 cd ansible
+git checkout v2.0.2.0-1
 git submodule update --init --recursive
 make install
 rm -fr ~/ansible
